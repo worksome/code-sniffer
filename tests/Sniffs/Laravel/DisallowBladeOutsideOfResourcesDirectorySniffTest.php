164f4e3154548f46d40ee2fc101de05a4b20d022
luke <lukeraymonddowning@gmail.com>
2022-06-07 14:21:44 +0200

    This PR adds a new `DisallowBladeOutsideOfResourcesDirectorySniff`, which throws an error if a blade file is placed outside of Laravel's resources directory.

--- tests/Sniffs/Laravel/DisallowBladeOutsideOfResourcesDirectorySniffTest.php
+++ tests/Sniffs/Laravel/DisallowBladeOutsideOfResourcesDirectorySniffTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Worksome\WorksomeSniff\Tests\Sniffs\Laravel;
 
 use Worksome\WorksomeSniff\Sniffs\Laravel\DisallowBladeOutsideOfResourcesDirectorySniff;
